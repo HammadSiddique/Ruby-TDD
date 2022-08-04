f8d7d5fc16e6212d4a6e26bfcf768976ca9c8f95
HammadSiddique <hammad.siddique01@gmail.com>
2022-08-04 17:55:12 +0500

    Write factorial method using TDD

---
+++ solver.rb
@@ -0,0 +1,12 @@
+class Solver
+  def factorial(num)
+    if num.negative?
+      'Please use a positive number'
+
+    elsif [1, 0].include?(num)
+      1
+    else
+      num * factorial(num - 1)
+    end
+  end
+end
--- spec/factorial_spec.rb
+++ spec/factorial_spec.rb
@@ -3,11 +3,16 @@ require_relative '../solver'
 describe Solver do
   context 'The factorial method returns the factorial of the integer' do
     it 'Factorial of 5 should return 120' do
-      solver = Solver.new
-        expect(solver.factorial(5)).to eql(120)
+      @solver = Solver.new
+      expect(@solver.factorial(5)).to eql(120)
     end
     it 'Factorial of 0 should return 1' do
-        expect(solver.factorial(0)).to eql(1)
+      @solver = Solver.new
+      expect(@solver.factorial(5)).to eql(120)
+    end
+    it 'Factorial of negative number should return error' do
+      @solver = Solver.new
+      expect(@solver.factorial(-8)).to eql('Please use a positive number')
     end
   end
 end
