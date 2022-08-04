1cc0169070bbc2cf3d9f7a230ad96a10cd4bfb24
HammadSiddique <hammad.siddique01@gmail.com>
2022-08-04 17:27:39 +0500

    Create tests for factorial

--- spec/factorial_spec.rb
+++ spec/factorial_spec.rb
@@ -2,10 +2,12 @@ require_relative '../solver'
 
 describe Solver do
   context 'The factorial method returns the factorial of the integer' do
-    it 'returns the factorial of the given number' do
+    it 'Factorial of 5 should return 120' do
       solver = Solver.new
-
-      expect(solver.factorial(5)).to eql(120)
+        expect(solver.factorial(5)).to eql(120)
+    end
+    it 'Factorial of 0 should return 1' do
+        expect(solver.factorial(0)).to eql(1)
     end
   end
 end
